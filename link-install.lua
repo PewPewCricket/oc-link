19d680b48c9405cb436013fce4714486aa73c1a4
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2024-07-02 22:44:34 -0500

    Update link-install.lua

--- link-install.lua
+++ link-install.lua
@@ -1,5 +1,7 @@
 local shell = require("shell")
 
 shell.execute("mkdir /etc/link.d")
-shell.execute("wget https://raw.githubusercontent.com/PewPewCricket/oc-link/main/link.lua /etc/rc.d/link.lua")
+shell.execute("wget https://raw.githubusercontent.com/PewPewCricket/oc-link/main/linklib.lua /lib/link.lua")
+shell.execute("wget https://raw.githubusercontent.com/PewPewCricket/oc-link/main/linkrc.lua /etc/rc.d/link.lua")
+shell.execute("wget https://raw.githubusercontent.com/PewPewCricket/oc-link/main/link.lua /bin/link.lua")
 shell.execute("rc link enable")
