6b679650baa3a0baa7943a66e23fc52ae94613d8
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2024-07-18 08:41:03 -0500

    Update ln1.lua

--- bin/ln1.lua
+++ bin/ln1.lua
@@ -1,14 +1,14 @@
 local shell = require("shell")
-local ld = require("ld")
+local link = require("link")
 local futils = require("tools/futils")
 local fs = require("filesystem")
 local args, ops = shell.parse(...)
 
 if ops.r then     --Remove a symlink
   if args[1] == nil then
-    shell.execute("man ld")
-  elseif futils.findData(shell.resolve(args[1]), futils.fileToTable("/etc/ld.lst")) ~= nil then
-    ld.remove(shell.resolve(args[1]))
+    shell.execute("man ln")
+  elseif futils.findData(shell.resolve(args[1]), futils.fileToTable("/etc/link.lst")) ~= nil then
+    link.remove(shell.resolve(args[1]))
     print(string.format("removed link from list: %s", shell.resolve(args[1])))
   elseif fs.exists(shell.resolve(args[1])) and fs.isLink(shell.resolve(args[1])) then
     fs.remove(shell.resolve(args[1]))
@@ -18,11 +18,11 @@ if ops.r then     --Remove a symlink
   end
 else              --Create a permanant symlink
   if args[1] == nil or args[2] == nil then
-    shell.execute("man ld")
+    shell.execute("man ln")
   elseif fs.exists(shell.resolve(args[2])) then
     io.stderr:write(string.format("file already exists: %s", shell.resolve(args[1])))
   elseif fs.exists(shell.resolve(args[1])) then
-    ld.create(shell.resolve(args[1]), shell.resolve(args[2]))
+    link.create(shell.resolve(args[1]), shell.resolve(args[2]))
     print(string.format("added link to list: %s > %s", shell.resolve(args[2]), shell.resolve(args[1])))
   else
     io.stderr:write(string.format("can't link to file: %s", shell.resolve(args[1])))
