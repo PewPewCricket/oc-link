5f68e707be594cfd613e0b7110185fc4c481fe04
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2024-07-18 08:39:30 -0500

    Update and rename ld.lua to link.lua

--- lib/link.lua
+++ lib/link.lua
@@ -4,10 +4,10 @@ local lib = {}
 
 function lib.remove(link)
   fs.remove(link)
-  local fileData = futils.fileToTable("/etc/ld.lst")
+  local fileData = futils.fileToTable("/etc/link.lst")
   local removeIndex = futils.findData(link, fileData)
   table.remove(fileData, removeIndex)
-  file = io.open("/etc/ld.lst", "w")
+  file = io.open("/etc/link.lst", "w")
   for i, v in ipairs(fileData) do
     file:write(v .. "\n")
   end
@@ -16,7 +16,7 @@ end
 
 function lib.create(target, link)
   fs.link(target, link)
-  local file = io.open("/etc/ld.lst", "a")
+  local file = io.open("/etc/link.lst", "a")
   file:write(string.format("%s > %s\n", link, target))
   file:close()
   return
