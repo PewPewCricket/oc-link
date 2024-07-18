c01e27e912e87e2d7b522f79feffa24167ed2835
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2024-07-18 08:20:33 -0500

    Update ld.lua

--- bin/ld.lua
+++ bin/ld.lua
@@ -1,14 +1,14 @@
 local shell = require("shell")
-local link = require("link")
-local ftable = require("tools/ftable")
+local ld = require("ld")
+local futils = require("tools/futils")
 local fs = require("filesystem")
 local args, ops = shell.parse(...)
 
 if ops.r then     --Remove a symlink
   if args[1] == nil then
     shell.execute("man link")
-  elseif ftable.findData(shell.resolve(args[1]), ftable.fileToTable("/etc/link.d/links.list")) ~= nil then
-    link.remove(shell.resolve(args[1]))
+  elseif futils.findData(shell.resolve(args[1]), futils.fileToTable("/etc/ld.lst")) ~= nil then
+    ld.remove(shell.resolve(args[1]))
     print(string.format("removed link from list: %s", shell.resolve(args[1])))
   elseif fs.exists(shell.resolve(args[1])) and fs.isLink(shell.resolve(args[1])) then
     fs.remove(shell.resolve(args[1]))
@@ -22,7 +22,7 @@ elseif ops.t then --Create a temporary symlink
   elseif fs.exists(shell.resolve(args[1])) then
     io.stderr:write(string.format("file already exists: %s", shell.resolve(args[1])))
   elseif fs.exists(shell.resolve(args[2])) then
-    link.createTemp(shell.resolve(args[1]), shell.resolve(args[2]))
+    ld.createTemp(shell.resolve(args[1]), shell.resolve(args[2]))
     print(string.format("created temporary link: %s > %s", shell.resolve(args[1]), shell.resolve(args[2])))
   else
     io.stderr:write(string.format("can't link to file: %s", shell.resolve(args[2])))
@@ -33,7 +33,7 @@ else              --Create a permanant symlink
   elseif fs.exists(shell.resolve(args[1])) then
     io.stderr:write(string.format("file already exists: %s", shell.resolve(args[1])))
   elseif fs.exists(shell.resolve(args[2])) then
-    link.create(shell.resolve(args[1]), shell.resolve(args[2]))
+    ld.create(shell.resolve(args[1]), shell.resolve(args[2]))
     print(string.format("added link to list: %s > %s", shell.resolve(args[1]), shell.resolve(args[2])))
   else
     io.stderr:write(string.format("can't link to file: %s", shell.resolve(args[2])))
